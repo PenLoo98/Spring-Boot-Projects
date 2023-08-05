01f0874116965dd921ce047ce5fd44090a60b5b0
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-08-06 02:57:17 +0900

    the spring and next.js application run at the same time
    by using CommandLineRunner

--- healthdiary/src/main/java/com/gachon/healthdiary/HealthdiaryApplication.java
+++ healthdiary/src/main/java/com/gachon/healthdiary/HealthdiaryApplication.java
@@ -1,7 +1,14 @@
 package com.gachon.healthdiary;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @SpringBootApplication
 public class HealthdiaryApplication {
@@ -10,4 +17,24 @@ public class HealthdiaryApplication {
 		SpringApplication.run(HealthdiaryApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runNextJsApp() {
+		return args -> {
+			ProcessBuilder processBuilder = new ProcessBuilder("npm","run","dev");
+			processBuilder.directory(new File("/Users/penloo/Documents/GitHub/Lecture-Management/src/manageFE"));
+			try {
+				Process process = processBuilder.start();
+
+				// Optional: Print the output of the external command
+				BufferedReader reader =
+						new BufferedReader(new InputStreamReader(process.getInputStream()));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					System.out.println(line);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		};
+	}
 }
