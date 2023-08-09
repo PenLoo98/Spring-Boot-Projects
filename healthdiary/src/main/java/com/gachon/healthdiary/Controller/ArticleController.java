f128b6e4cac676cecf2279ab252c9219444f8af8
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-08-09 21:45:47 +0900

    show commentlist and CREATE comment

--- healthdiary/src/main/java/com/gachon/healthdiary/Controller/ArticleController.java
+++ healthdiary/src/main/java/com/gachon/healthdiary/Controller/ArticleController.java
@@ -1,13 +1,18 @@
 package com.gachon.healthdiary.Controller;
 
 import com.gachon.healthdiary.DTO.ArticleForm;
+import com.gachon.healthdiary.DTO.CommentDTO;
 import com.gachon.healthdiary.Entity.Article;
 import com.gachon.healthdiary.Repository.ArticleRepository;
+import com.gachon.healthdiary.Service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -18,6 +23,8 @@ import java.util.List;
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+    @Autowired
+    private CommentService commentService;
 
     // articles 메인페이지
     @GetMapping("")
@@ -66,9 +73,12 @@ public class ArticleController {
         // 1. id를 조회해 DB에서 해당 데이터 가져오기
 //        Optional<Article> articleEntity = articleRepository.findById(id);
         Article foundArticle = articleRepository.findById(id).orElse(null);
+        List<CommentDTO> commentsDTOs = commentService.getComments(id);
 
         // 2. 모델에 가져온 데이터 등록하기
         model.addAttribute("article", foundArticle);
+        model.addAttribute("commentsDTOs", commentsDTOs);
+
         // 조회한 데이터를 사용자에게 보여주기 위한 뷰페이지 만들고 변환하기
         return "articles/show";
     }
