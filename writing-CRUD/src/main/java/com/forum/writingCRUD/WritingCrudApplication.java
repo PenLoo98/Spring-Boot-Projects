868c56da34a4a67b905fc445ddc38e7053e9ea8a
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-09-09 17:01:56 +0900

    add the writingCRUD project

---
+++ writing-CRUD/src/main/java/com/forum/writingCRUD/WritingCrudApplication.java
@@ -0,0 +1,13 @@
+package com.forum.writingCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritingCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WritingCrudApplication.class, args);
+	}
+
+}
